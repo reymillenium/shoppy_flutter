00dfaa15affdd77ececf9e174b00f0a2649d9c0e
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-06-20 18:42:49 -0400

    Modified the GridTile on the lib/components/product_grid_tile.dart, with a Material class. And then added an elevation

--- lib/components/product_grid_tile.dart
+++ lib/components/product_grid_tile.dart
@@ -31,18 +31,6 @@ class ProductGridTile extends StatelessWidget {
   final currencyFormat = new NumberFormat("#,##0.00", "en_US");
 
   void selectProduct(BuildContext context) {
-    // Navigator.of(context).push(
-    //   MaterialPageRoute(
-    //     builder: (context) => ProductShowScreen(
-    //       appTitle: 'Feeddy',
-    //       product: product,
-    //     ),
-    //   ),
-    // );
-
-    //  Named route:
-    // Navigator.pushNamed(context, ProductShowScreen.screenId, arguments: ProductShowScreenArguments(product));
-    // It can even use a Map instead:
     Navigator.pushNamed(context, ProductShowScreen.screenId, arguments: {'product': product});
   }
 
@@ -55,12 +43,12 @@ class ProductGridTile extends StatelessWidget {
     Function onDeleteProductHandler = (id, context) => productsData.deleteProductWithConfirm(id, context);
     Function toggleFavorite = (userId, productId) => productsData.toggleFavorite(userId, productId);
 
-    final String formattedDate = formatter.format(product.createdAt);
+    // final String formattedDate = formatter.format(product.createdAt);
     final String amountLabel = '${currentCurrency['symbol']}${currencyFormat.format(product.price)}';
     final double priceFontSize = (84 / amountLabel.length);
 
     Color primaryColor = Theme.of(context).primaryColor;
-    var foregroundColor = ColorHelper.contrastingColor(primaryColor);
+    // var foregroundColor = ColorHelper.contrastingColor(primaryColor);
 
     return FutureBuilder(
         future: productsData.isFavorite(1, product.id),
@@ -72,161 +60,160 @@ class ProductGridTile extends StatelessWidget {
 
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return GridTile(
-                child: Stack(
-                  children: [
-                    // Product Image:
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
+              return Material(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                elevation: 2,
+                child: GridTile(
+                  child: Stack(
+                    children: [
+                      // Product Image:
+                      ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: Image.network(
+                          product.imageUrl,
+                          height: 250,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: Image.network(
-                        product.imageUrl,
-                        height: 250,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
 
-                    // Product Price:
-                    ...[
-                      Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Container(
-                          height: 26,
-                          padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-                          decoration: BoxDecoration(
-                            color: Colors.black38,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
+                      // Product Price:
+                      ...[
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          child: Container(
+                            height: 26,
+                            padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.black38,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
                             ),
-                          ),
-                          child: FittedBox(
-                            child: Text(
-                              '$amountLabel',
-                              style: TextStyle(
-                                color: ColorHelper.contrastingColor(primaryColor),
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.italic,
-                                fontSize: priceFontSize,
+                            child: FittedBox(
+                              child: Text(
+                                '$amountLabel',
+                                style: TextStyle(
+                                  color: ColorHelper.contrastingColor(primaryColor),
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: priceFontSize,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
 
-                    // Action Buttons (Delete & Edit):
-                    ...[
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Container(
-                          height: 26,
-                          padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-                          decoration: BoxDecoration(
-                            color: Colors.black38,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomLeft: Radius.circular(10),
+                      // Action Buttons (Delete & Edit):
+                      ...[
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Container(
+                            height: 26,
+                            padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.black38,
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                              ),
                             ),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Tooltip(
-                                  message: 'Delete',
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Tooltip(
+                                    message: 'Delete',
+                                    child: GestureDetector(
+                                      child: Container(
+                                        margin: EdgeInsets.only(right: 8),
+                                        child: Icon(
+                                          Icons.delete,
+                                          color: Colors.white,
+                                          size: 14,
+                                        ),
+                                      ),
+                                      onTap: () => onDeleteProductHandler(product.id, context),
+                                    )),
+                                Tooltip(
+                                  message: 'Edit',
                                   child: GestureDetector(
                                     child: Container(
-                                      margin: EdgeInsets.only(right: 8),
+                                      margin: EdgeInsets.only(left: 8),
                                       child: Icon(
-                                        Icons.delete,
+                                        Icons.edit,
                                         color: Colors.white,
                                         size: 14,
                                       ),
                                     ),
-                                    onTap: () => onDeleteProductHandler(product.id, context),
-                                  )),
-                              Tooltip(
-                                message: 'Edit',
-                                child: GestureDetector(
-                                  child: Container(
-                                    margin: EdgeInsets.only(left: 8),
-                                    child: Icon(
-                                      Icons.edit,
-                                      color: Colors.white,
-                                      size: 14,
-                                    ),
+                                    onTap: () {
+                                      showModalBottomSheet(
+                                        backgroundColor: Colors.transparent,
+                                        isScrollControlled: true,
+                                        context: context,
+                                        builder: (context) => ProductEditScreen(
+                                          product: product,
+                                        ),
+                                      );
+                                    },
                                   ),
-                                  onTap: () {
-                                    showModalBottomSheet(
-                                      backgroundColor: Colors.transparent,
-                                      isScrollControlled: true,
-                                      context: context,
-                                      builder: (context) => ProductEditScreen(
-                                        product: product,
-                                      ),
-                                    );
-                                  },
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ],
-                  ],
-                ),
-
-                // Footer: Favorite icon, Title & Add to cart icon
-                footer: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    // topLeft: Radius.circular(10),
-                    // topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
                   ),
-                  child: GridTileBar(
-                    backgroundColor: Colors.black38,
 
-                    // Favorite icon:
-                    leading: IconButton(
-                      // iconSize: 24,
-                      icon: Icon(
-                        isFavorite ? Icons.favorite : Icons.favorite_border,
-                        color: isFavorite ? Colors.yellow : Colors.white,
-                        size: 14,
-                      ),
-                      tooltip: 'Favorite',
-                      onPressed: () => toggleFavorite(1, product.id),
+                  // Footer: Favorite icon, Title & Add to cart icon
+                  footer: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      // topLeft: Radius.circular(10),
+                      // topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
                     ),
+                    child: GridTileBar(
+                      backgroundColor: Colors.black38,
 
-                    // Product Title:
-                    title: Text(
-                      product.title,
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-
-                    // Add to cart icon:
-                    trailing: Tooltip(
-                      padding: EdgeInsets.all(0),
-                      message: 'Add to cart',
-                      child: IconButton(
-                        padding: EdgeInsets.all(0),
+                      // Favorite icon:
+                      leading: IconButton(
+                        // iconSize: 24,
                         icon: Icon(
-                          Icons.shopping_cart_outlined,
-                          color: Colors.white,
+                          isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: isFavorite ? Colors.yellow : Colors.white,
                           size: 14,
                         ),
-                        onPressed: () => {},
+                        tooltip: 'Favorite',
+                        onPressed: () => toggleFavorite(1, product.id),
+                      ),
+
+                      // Product Title:
+                      title: Text(
+                        product.title,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+                      // Add to cart icon:
+                      trailing: Tooltip(
+                        padding: EdgeInsets.all(0),
+                        message: 'Add to cart',
+                        child: IconButton(
+                          padding: EdgeInsets.all(0),
+                          icon: Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                            size: 14,
+                          ),
+                          onPressed: () => {},
+                        ),
                       ),
                     ),
                   ),

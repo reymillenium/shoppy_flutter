afab001fa4b3f016757f04b79b7755cdd70f3cb1
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-07-12 22:19:07 -0400

    Wrapped all the fields on a form at lib/screens/product_edit_screen.dart

--- lib/screens/product_edit_screen.dart
+++ lib/screens/product_edit_screen.dart
@@ -20,6 +20,7 @@ class ProductEditScreen extends StatefulWidget {
 
   // Properties:
   final Product product;
+
   // final Function onUpdateProductHandler;
 
   // Constructor:
@@ -38,6 +39,7 @@ class _ProductEditScreenState extends State<ProductEditScreen> {
   String _title = '';
   String _description = '';
   double _price = 0;
+
   // String _imageUrl = ListHelper.randomFromList(DUMMY_FOOD_IMAGE_URLS);
   String _imageUrl = PRODUCT_PLACEHOLDER_IMAGE;
 
@@ -87,166 +89,168 @@ class _ProductEditScreenState extends State<ProductEditScreen> {
       bottom: false,
       child: SingleChildScrollView(
         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom), // !important
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+        child: Form(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 30.0),
-            child: Column(
-              children: [
-                Text(
-                  'Update Product',
-                  style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 30,
-                  ),
-                ),
-
-                // Title Input
-                TextFormField(
-                  initialValue: _title,
-                  autofocus: true,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                      ),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: primaryColor,
-                        width: 4.0,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: accentColor,
-                        width: 6.0,
-                      ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Update Product',
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 30,
                     ),
                   ),
-                  textInputAction: TextInputAction.next,
-                  style: TextStyle(),
-                  onChanged: (String newText) {
-                    setState(() {
-                      _title = newText;
-                    });
-                  },
-                  // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
-                  onFieldSubmitted: (String inputValue) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  },
-                ),
 
-                // Price Input
-                TextFormField(
-                  initialValue: _price.toString(),
-                  autofocus: true,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    labelText: 'Price',
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red,
+                  // Title Input
+                  TextFormField(
+                    initialValue: _title,
+                    autofocus: true,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: primaryColor,
+                          width: 4.0,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: accentColor,
+                          width: 6.0,
+                        ),
                       ),
                     ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: primaryColor,
-                        width: 4.0,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: accentColor,
-                        width: 6.0,
-                      ),
-                    ),
+                    textInputAction: TextInputAction.next,
+                    style: TextStyle(),
+                    onChanged: (String newText) {
+                      setState(() {
+                        _title = newText;
+                      });
+                    },
+                    // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
+                    onFieldSubmitted: (String inputValue) {
+                      FocusScope.of(context).requestFocus(_priceFocusNode);
+                    },
                   ),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
-                  focusNode: _priceFocusNode,
-                  style: TextStyle(),
-                  onChanged: (String newText) {
-                    setState(() {
-                      _price = NumericHelper.roundDouble(newText.parseDoubleOrZero, 2);
-                    });
-                  },
-                  // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
-                  onFieldSubmitted: (String inputValue) {
-                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
-                  },
-                ),
 
-                // Description Input
-                TextFormField(
-                  initialValue: _description,
-                  autofocus: true,
-                  autocorrect: false,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red,
+                  // Price Input
+                  TextFormField(
+                    initialValue: _price.toString(),
+                    autofocus: true,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      labelText: 'Price',
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: primaryColor,
+                          width: 4.0,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: accentColor,
+                          width: 6.0,
+                        ),
                       ),
                     ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: primaryColor,
-                        width: 4.0,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: accentColor,
-                        width: 6.0,
-                      ),
-                    ),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
+                    focusNode: _priceFocusNode,
+                    style: TextStyle(),
+                    onChanged: (String newText) {
+                      setState(() {
+                        _price = NumericHelper.roundDouble(newText.parseDoubleOrZero, 2);
+                      });
+                    },
+                    // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
+                    onFieldSubmitted: (String inputValue) {
+                      FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                    },
                   ),
-                  keyboardType: TextInputType.multiline,
-                  focusNode: _descriptionFocusNode,
-                  style: TextStyle(),
-                  onChanged: (String newText) {
-                    setState(() {
-                      _description = newText;
-                    });
-                  },
-                  // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
-                ),
 
-                // Update button:
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 30.0),
-                  child: Material(
-                    color: _hasValidData() ? primaryColor : Colors.grey,
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      disabledColor: Colors.grey,
-                      onPressed: _hasValidData() ? () => _updateData(context, onUpdateProductsHandler) : null,
-                      minWidth: double.infinity,
-                      height: 42.0,
-                      child: Text(
-                        'Update',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                          color: Colors.white,
+                  // Description Input
+                  TextFormField(
+                    initialValue: _description,
+                    autofocus: true,
+                    autocorrect: false,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: primaryColor,
+                          width: 4.0,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: accentColor,
+                          width: 6.0,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    focusNode: _descriptionFocusNode,
+                    style: TextStyle(),
+                    onChanged: (String newText) {
+                      setState(() {
+                        _description = newText;
+                      });
+                    },
+                    // onFieldSubmitted: !_hasValidData() ? null : (_) => () => _updateData(context, onUpdateProductsHandler),
+                  ),
+
+                  // Update button:
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 30.0),
+                    child: Material(
+                      color: _hasValidData() ? primaryColor : Colors.grey,
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        disabledColor: Colors.grey,
+                        onPressed: _hasValidData() ? () => _updateData(context, onUpdateProductsHandler) : null,
+                        minWidth: double.infinity,
+                        height: 42.0,
+                        child: Text(
+                          'Update',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
